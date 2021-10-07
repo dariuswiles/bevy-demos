eb1a2593ff60873fb8161feddc106278ae002fd7
Darius Wiles <github@dariuswiles.com>
2021-10-07 10:07:55 -0700

    Demo that rotates an entity

---
+++ src/bin/rotating_cube.rs
@@ -0,0 +1,68 @@
+/// Create two cubes, a light source, and a camera, and position them such that the main cube is
+/// visible from the camera. The main cube rotates, the other doesn't. This is controlled by
+/// adding the `RotatingEntity` component to the main cube. Theoretically, the `rotate_entities`
+/// system should rotate all types of entities that have this component.
+
+use bevy::prelude::*;
+
+struct RotatingEntity;  // Component to indicate entity should be rotated
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Create and add a default material
+    let material_handle = materials.add(StandardMaterial {
+        base_color: Color::rgb(0.7, 0.6, 0.7),
+        ..Default::default()
+    });
+
+    // Location to put cube
+    let cube_location = Vec3::new(0.0, 0.0, 4.0);
+
+    // Create a mesh from a `Cube` `shape`
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
+        material: material_handle.clone(),
+        transform: Transform::from_translation(cube_location),
+        ..Default::default()
+    }).insert(RotatingEntity);
+
+    // Another cube, this time without `RotatingEntity`.
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube::new(0.5))),
+        material: material_handle.clone(),
+        transform: Transform::from_translation(Vec3::new(1.5, 0.0, 6.0)),
+        ..Default::default()
+    });
+
+    // Light
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_xyz(2.0, 5.0, 2.0),
+        ..Default::default()
+    });
+
+    // Camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(1.0, 2.0, 0.0)
+            .looking_at(cube_location, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+
+fn rotate_entities(time: Res<Time>, mut query: Query<&mut Transform, With<RotatingEntity>>) {
+    for mut transform in query.iter_mut() {
+        transform.rotation = Quat::from_rotation_y(time.seconds_since_startup() as f32);
+    }
+}
+
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_system(rotate_entities.system())
+        .run();
+}
