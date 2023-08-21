10b61a5dfc6fe8c45265c2590d97dd5842ce90cc
Darius Wiles <github@dariuswiles.com>
2023-08-21 11:10:53 -0700

    Remove explicitly opened window as it's no longer required

--- src/bin/window_containing_text.rs
+++ src/bin/window_containing_text.rs
@@ -2,8 +2,6 @@
 
 use bevy::prelude::*;
 
-const WINDOW_WIDTH: f32 = 1000.0;
-const WINDOW_HEIGHT: f32 = 400.0;
 const FONT_ASSET_FILENAME: &str = "fonts/FiraSans-Bold.ttf";
 
 // This is used to prevent the program from exiting immediately
@@ -24,32 +22,18 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                         color: Color::RED,
                     },
                 ),
-                transform: Transform::from_xyz(-400.0, 200.0, 1.0),
+                transform: Transform::from_xyz(0.0, 200.0, 1.0),
                 ..Default::default()
             }
         );
 }
 
 fn main() {
-    let wd = WindowDescriptor {
-        width: WINDOW_WIDTH,
-        height: WINDOW_HEIGHT,
-        title: String::from("Display test text in a window"),
-        ..Default::default()
-    };
-
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: wd,
             ..default()
         }))
         .add_startup_system(setup)
         .add_system(empty_system)
         .run();
-//     App::new()
-//         .insert_resource(wd)
-//         .add_plugins(DefaultPlugins)
-//         .add_startup_system(setup)
-//         .add_system(empty_system)
-//         .run();
 }
